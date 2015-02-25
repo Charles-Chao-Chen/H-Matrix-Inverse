4df30c189783062f0940cb44ed641030343128cf
Chao Chen <cchen10@stanford.edu>
2015-02-25 15:01:21 -0800

    added class Timer; simplify Makefile

--- include/timer.hpp
+++ include/timer.hpp
@@ -1,15 +1,43 @@
 #ifndef timer_h
 #define timer_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <unistd.h>
+#include <sys/time.h>
+#include <iostream>
 
-/* Return time in seconds since the Unix epoch */
+// Return time in seconds since the Unix epoch
 double timer(void);
 
-#ifdef __cplusplus
+class Timer {
+public:
+  void start();
+  void stop();
+  void get_elapsed_time();
+private:
+  double tStart;
+  double tStop;
+};
+
+inline void Timer::start() {
+  tStart = timer();
 }
-#endif
+
+inline void Timer::stop() {
+  tStop = timer();
+}
+
+inline void Timer::get_elapsed_time() {
+  std::cout << "Elapsed time : " << tStop-tStart << std::endl;
+}
+
+inline double timer(void)
+{
+  double time;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  time = (double)tv.tv_sec + (double)tv.tv_usec/1.e6;
+  return time;
+}
+
 
 #endif /* timer_h */
