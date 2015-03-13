a9b2188ead49b9b1998d9ca07677fac0ec2b1485
Chao Chen <cchen10@stanford.edu>
2015-03-13 11:23:13 -0700

    added get_elapsed_time(const char*)

--- include/timer.hpp
+++ include/timer.hpp
@@ -1,43 +1,15 @@
 #ifndef timer_h
 #define timer_h
 
-#include <unistd.h>
-#include <sys/time.h>
-#include <iostream>
-
-// Return time in seconds since the Unix epoch
-double timer(void);
-
 class Timer {
 public:
   void start();
   void stop();
   void get_elapsed_time();
+  void get_elapsed_time(const char*);
 private:
   double tStart;
   double tStop;
 };
-
-inline void Timer::start() {
-  tStart = timer();
-}
-
-inline void Timer::stop() {
-  tStop = timer();
-}
-
-inline void Timer::get_elapsed_time() {
-  std::cout << "Elapsed time : " << tStop-tStart << std::endl;
-}
-
-inline double timer(void)
-{
-  double time;
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  time = (double)tv.tv_sec + (double)tv.tv_usec/1.e6;
-  return time;
-}
-
 
 #endif /* timer_h */
---
+++ src/timer.cpp
@@ -0,0 +1,32 @@
+#include "timer.hpp"
+
+#include <unistd.h>
+#include <sys/time.h>
+#include <iostream>
+
+// Return time in seconds since the Unix epoch
+static double timer() {
+double time;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  time = (double)tv.tv_sec + (double)tv.tv_usec/1.e6;
+  return time;
+}
+
+void Timer::start() {
+  tStart = timer();
+}
+
+void Timer::stop() {
+  tStop = timer();
+}
+
+void Timer::get_elapsed_time() {
+std::cout << "Elapsed time : " << tStop-tStart << std::endl;
+}
+
+void Timer::get_elapsed_time(const char* msg) {
+std::cout << msg << " takes : "
+<< tStop-tStart << " seconds" << std::endl;
+}
+
