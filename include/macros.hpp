2aee1d73d157c96a26da93ff05a1bd935678d3dd
Chao Chen <cchen10@stanford.edu>
2015-03-14 21:01:55 -0700

    added ErrorMessage macro

--- include/macros.hpp
+++ include/macros.hpp
@@ -4,14 +4,22 @@
 #include <Eigen/Dense>
 typedef Eigen::MatrixXd EMatrix;
 
+// constants used in ComputeLowRank_SVD()
 #include <float.h>
 const double SVD_ZERO_TOL = DBL_EPSILON * 1e2;
-
 const double SVD_RANK_TOL = DBL_EPSILON * 1e3;
 
+// admissibility type
 enum AdmissType {
   WEAK,
   STRONG,
 };
+
+// error message
+#define ErrorMessage(msg) std::cerr		\
+  << "Error in file : " << __FILE__		\
+  << ", function : " << __func__		\
+  << ", line : " << __LINE__			\
+  << "\n\t" << msg << std::endl
 
 #endif // MACROS_H
